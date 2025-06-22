1de32e0a59dbb0c20638be008c0e51eaa2d8f3cc
Alex Martínez de Francisco <alexmartinezdefrancisco@gmail.com>
2025-06-22 19:50:12 +0200

    Update Python version to 3.12, streamline dependencies in environment.yml and requirements.txt, and enhance setup.py with long description and correct package details.

--- setup.py
+++ setup.py
@@ -1,26 +1,33 @@
 from setuptools import setup, find_packages
 
+# Read the contents of your README file
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
 setup(
     name='fin404-project',
     version='1.0.0',
     description='EPFL Fin404 Derivatives Project: The VIX and related derivatives',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     author='Gabriele Calandrino, Alex Martinez de Francisco, Federico Sabbatani Schiuma, Letizia Seveso',
-    packages=find_packages(where='src'),
-    package_dir={'': 'src'},
+    packages=find_packages(),
     install_requires=[
-        # List dependencies from environment.yml or requirements.txt
-        # if you intend this to be a standalone package.
-        # e.g., 'numpy', 'pandas', 'scipy', 'matplotlib'
+        'numpy',
+        'scipy',
+        'matplotlib',
+        'tqdm'
     ],
     entry_points={
         'console_scripts': [
-            'fin405=main:main',
+            'fin404=main:main',
         ],
     },
     include_package_data=True,
     classifiers=[
-        'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
         'License :: OSI Approved :: MIT License',
-        'Operating System :: OS Independent',
+        'Operating System :: OS Independent'
     ]
 )
